dc8d83fbdca1adc8a6748653860ff3630eb5477b
ysphotodev <ysphotodev@gmail.com>
2024-05-20 19:32:34 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -21,6 +21,25 @@ const nextConfig = {
 
 
 
+
+
+async headers() {
+        return [
+            {
+                source: "*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "false" },
+                    { key: "Access-Control-Allow-Origin", value: "https://server-two-lilac.vercel.app/" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
+                ]
+            }
+        ]
+    }
+
+  
+
+
 };
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
