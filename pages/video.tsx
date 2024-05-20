17d502eba34e3f8e72dda875478f72c4abf0f7bc
ysphotodev <ysphotodev@gmail.com>
2024-05-20 22:32:30 +0300

    Update video.tsx

--- pages/video.tsx
+++ pages/video.tsx
@@ -261,17 +261,19 @@ export default function Home() {
                 allowFullScreen
               />
 
-              <iframe
-                className="max-w-[500px]"
-                width="560"
-                height="315"
-                src="https://www.youtube.com/watch?v=79htoTvO4lI&t=631s"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen
-              />
+              <iframe 
+                className="max-w-[500px]" 
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/79htoTvO4lI?si=oTDQ8o0Obo1armDW" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen>
+              </iframe>
+
+            
               <iframe
                 className="max-w-[500px]"
                 width="560"
