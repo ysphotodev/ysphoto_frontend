df5321c14bcdc94e3a073b6c4949bc3c70640c1c
ysphotodev <ysphotodev@gmail.com>
2024-06-03 21:05:33 +0300

    add updated

--- next.config.js
+++ next.config.js
@@ -38,7 +38,7 @@ const nextConfig = {
         return [
             {
                 source: '/admin',
-                destination: 'https://64.176.70.84:1337/admin',
+                destination: 'http://64.176.70.84:1337/admin',
                 permanent: true,
             },
         ]
