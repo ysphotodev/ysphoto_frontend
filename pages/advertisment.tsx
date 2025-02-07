3dc3de96001d05f25c2e6f51e7811c0fa4619cbe
Rocolusso <prolight@prolights-MacBook-Pro.local>
2025-02-07 22:17:11 +0200

    update advertisment.tsx

--- pages/advertisment.tsx
+++ pages/advertisment.tsx
@@ -12,6 +12,19 @@ import logo from '../public/assets/logo_ys_od_.svg';
 import ContactForm from "@/components/ContactForm";
 
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import Modal from 'react-modal'
+import "swiper/css";
+import "swiper/css/navigation";
+import 'swiper/css/pagination';
+
+Modal.setAppElement("#__next"); // Prevents screen readers issues
+
+
+
+
+
 export default function Home({ posts }:{posts:{
     attributes:{
       Title:string,
@@ -20,6 +33,18 @@ export default function Home({ posts }:{posts:{
     id:string
   }[]}) {
   const [isOpen, setIsOpen] = useState(false);
+
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const openModal = (index:number) => {
+    setSelectedIndex(index);
+    setIsOpenModal(true);
+  };
+
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
 
   return (
     <div>
@@ -222,28 +247,80 @@ export default function Home({ posts }:{posts:{
           <div className=" flex flex-col  ">
             <div className=" sm:my-3 py-5 bg-white  max-w-[600px]">
               <p
-                className={`text-center text-black notoSans text-[2.3rem] sm:text-[4.5rem] uppercase `}
+                  className={`text-center text-black notoSans text-[2.3rem] sm:text-[4.5rem] uppercase `}
               >
                 Advertisment
               </p>
 
             </div>
 
-            <div className="container grid grid-cols-2 sm:grid-cols-4 gap-3 mx-auto items-center">
+            {/*<div className="container grid  grid-cols-2 sm:grid-cols-4 gap-3 mx-auto items-center">*/}
 
-              {posts.map((post) => (
-                <Image
-                  className="hover:rounded-3xl hover:ease-in-out hover:tracking-tight hover:scale-125 hover:duration-75"
-                  key={post.id}
-                  src={post.attributes.URL}
-                  alt={post.attributes.Title}
-                  height={1000}
-                  width={1000}
-                  quality={80}
-                />
+            {/*  {posts.map((post) => (*/}
+            {/*    <Image*/}
+            {/*      className="w-full h-56 object-cover hover:rounded-3xl hover:ease-in-out hover:tracking-tight hover:scale-125 hover:duration-75"*/}
+            {/*      key={post.id}*/}
+            {/*      src={post.attributes.URL}*/}
+            {/*      alt={post.attributes.Title}*/}
+            {/*      height={1000}*/}
+            {/*      width={1000}*/}
+            {/*      quality={80}*/}
+            {/*    />*/}
+            {/*  ))}*/}
+
+            {/*</div>*/}
+
+
+            <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 p-4">
+              {posts.map((src, index) => (
+                  <Image
+                      key={index}
+                      src={src.attributes.URL}
+                      alt={`Thumbnail ${index}`}
+                      className="cursor-pointer w-full h-[350px] object-cover"
+                      onClick={() => openModal(index)}
+                      width={1000}
+                      height={1000}
+                  />
               ))}
-
             </div>
+
+
+            <Modal
+                isOpen={isOpenModal}
+                onRequestClose={closeModal}
+                contentLabel="Image Lightbox"
+                className="fixed z-50  inset-0 flex items-center justify-center bg-black bg-opacity-80 p-4"
+            >
+
+
+              <Swiper
+                  initialSlide={selectedIndex}
+                  navigation={true}
+                  pagination={true}
+                  // modules={[Navigation, Pagination]}
+                  modules={[Navigation]}
+                  // className="w-full max-w-3xl"
+                  className={'max-w-[500px]'}
+              >
+                {posts.map((src, index) => (
+                    <SwiperSlide key={index}>
+                      <button onClick={closeModal} className="z-50 absolute top-4 right-4 text-white text-3xl">
+                        ✖
+                      </button>
+                      <Image
+                          src={src.attributes.URL}
+                          alt={`Slide ${index}`}
+                          // className=" h-auto max-h-[80vh] mx-auto object-cover"
+                          className="object-cover mx-auto max-h-[80vh] "
+                          width={1000}
+                          height={1000}
+                      />
+                    </SwiperSlide>
+                ))}
+              </Swiper>
+            </Modal>
+
 
           </div>
 
@@ -253,7 +330,7 @@ export default function Home({ posts }:{posts:{
           <div className=" flex flex-col  ">
             <div className=" sm:my-3 py-5 bg-black  max-w-[600px] border border-2 border-black">
               <p
-                className={`text-center text-white notoSans text-[2.3rem] sm:text-[4.5rem] uppercase  `}
+                  className={`text-center text-white notoSans text-[2.3rem] sm:text-[4.5rem] uppercase  `}
               >
                 Contact Me
               </p>
